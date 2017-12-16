b1c7b6488b1e6afdf97bcfdba381f425ed3f2c0e
Tomasz Suchowierski <suchowierski@op.pl>
2017-12-16 15:38:59 +0100

    feat: zmiany

---
+++ gulpfile.js
@@ -0,0 +1,13 @@
+const gulp = require('gulp'); 
+const browserSync = require('browser-sync').create(); 
+
+gulp.task('serve', function() {
+  browserSync.init({
+    server: './',
+  }); 
+  gulp.watch('**/*.html').on('change', browserSync.reload);
+  gulp.watch('**/*.css').on('change', browserSync.reload);
+  gulp.watch('**/*.js').on('change', browserSync.reload);
+});
+
+gulp.task('default', ['serve']);
---
+++ js/main.js
@@ -0,0 +1,1 @@
+alert (uwaga1);
